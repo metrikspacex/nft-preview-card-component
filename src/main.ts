f0805a405adf390f75b0de6299125bf79d09e127
Matthew Mason <metrikspacex@gmail.com>
2023-05-08 22:41:04 -0400

    mobile finished

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,20 @@
 import "./main.scss";
+
+// const overlay = document.querySelector<HTMLDivElement>(".head-overlay");
+// const headImage = overlay?.querySelector(".head-image");
+// const headView = overlay?.querySelector(".head-view");
+
+// overlay?.addEventListener("mouseover", () => {
+//   headImage?.classList.add("head-image-hover-in");
+// });
+
+// overlay?.addEventListener("mouseout", () => {
+//   headImage?.classList.remove("head-image-hover-in");
+//   headImage?.classList.add("head-image-hover-out");
+//   headImage?.addEventListener("animationend", onAnimationEnd);
+// });
+
+// function onAnimationEnd() {
+//   headImage?.classList.remove("head-image-hover-in", "head-image-hover-out");
+//   headImage?.removeEventListener("animationend", onAnimationEnd);
+// }
